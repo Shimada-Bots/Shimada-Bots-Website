dfd9afe694bf943f715f610d30acdd822ac4af28
Bryan Silva <bryanjsilva04@gmail.com>
2023-09-11 21:00:09 -0700

    Finished teams slider

---
+++ webfiles/teams/script.js
@@ -0,0 +1,19 @@
+const teamSwitch = document.getElementById("switch");
+const teamSection = document.getElementById("teams");
+
+let currentTeam = 1;
+
+teamSwitch.addEventListener("click", () => {
+    if (currentTeam == 1)
+    {
+        teamSection.style.transform = "translate(-48.7%, 0)";
+        teamSwitch.style.backgroundColor = "var(--bg-color-dark)"
+        currentTeam = 2;
+    }
+    else if (currentTeam == 2)
+    {
+        teamSection.style.transform = "translate(0, 0)";
+        teamSwitch.style.backgroundColor = "var(--bg-color-light)"
+        currentTeam = 1;
+    }
+})
