c06bb53c79f232ca6efe28285ac1afbc7c0f244c
Bryan Silva <bryanjsilva04@gmail.com>
2023-09-13 14:22:07 -0700

    added buttons and gear animation

--- webfiles/about/chief_officers.js
+++ webfiles/about/chief_officers.js
@@ -1,1 +1,32 @@
+const leftButton = document.getElementById("left-co-button");
+const rightButton = document.getElementById("right-co-button");
+
+const largeGear = document.getElementById("large-gear");
+
 let dots = document.getElementsByClassName("dot");
+
+let currentCard = 0;
+
+function showSlides(n)
+{
+    currentCard = n;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      dots[currentCard].className += " active";
+    largeGear.style.transform = "rotate(" + (30*currentCard) + "deg)";
+}
+
+leftButton.addEventListener("click", () => {
+    if(currentCard > 0)
+    {
+        showSlides(currentCard-1);
+    }
+})
+
+rightButton.addEventListener("click", () => {
+    if(currentCard < 5)
+    {
+        showSlides(currentCard+1);
+    }
+})
