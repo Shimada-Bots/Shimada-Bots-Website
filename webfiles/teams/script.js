c902e353443a2d9c6d3dfc4e759e483136a9ce32
DBChrono <91212506+DBChrono@users.noreply.github.com>
2023-09-15 22:14:57 -0700

    Animation for teams

--- webfiles/teams/script.js
+++ webfiles/teams/script.js
@@ -1,5 +1,7 @@
 const teamSwitch = document.getElementById("switch");
 const teamSection = document.getElementById("teams");
+const aLine1 = document.getElementById("line1");
+const aLine2 = document.getElementById("line2");
 
 let currentTeam = 1;
 
@@ -7,13 +9,21 @@ teamSwitch.addEventListener("click", () => {
     if (currentTeam == 1)
     {
         teamSection.style.transform = "translate(-48.5%, 0)";
-        teamSwitch.style.backgroundColor = "var(--bg-color-dark)"
+        teamSwitch.style.backgroundColor = "var(--bg-color-dark)";
+        aLine1.style.backgroundColor = "var(--bg-color-light)";
+        aLine2.style.backgroundColor = "var(--bg-color-light)";
+        aLine1.style.transform = "rotate(360deg)";
+        aLine2.style.transform = "rotate(180deg)";
         currentTeam = 2;
     }
     else if (currentTeam == 2)
     {
         teamSection.style.transform = "translate(0, 0)";
-        teamSwitch.style.backgroundColor = "var(--bg-color-light)"
+        teamSwitch.style.backgroundColor = "var(--bg-color-light)";
+        aLine1.style.backgroundColor = "var(--bg-color-dark)";
+        aLine2.style.backgroundColor = "var(--bg-color-dark)";
+        aLine1.style.transform = "rotate(90deg)";
+        aLine2.style.transform = "rotate(-360deg)";
         currentTeam = 1;
     }
 })
