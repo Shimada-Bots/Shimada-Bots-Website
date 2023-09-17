a9dc9b49adc97acac054af48546935b4f1220195
DBChrono <91212506+DBChrono@users.noreply.github.com>
2023-09-16 21:24:29 -0700

    Teams Transitions and Animations

--- webfiles/teams/script.js
+++ webfiles/teams/script.js
@@ -2,6 +2,8 @@ const teamSwitch = document.getElementById("switch");
 const teamSection = document.getElementById("teams");
 const aLine1 = document.getElementById("line1");
 const aLine2 = document.getElementById("line2");
+const bot = document.getElementById("botName");
+const bot2 = document.getElementById("botName2");
 
 let currentTeam = 1;
 
@@ -14,6 +16,7 @@ teamSwitch.addEventListener("click", () => {
         aLine2.style.backgroundColor = "var(--bg-color-light)";
         aLine1.style.transform = "rotate(360deg)";
         aLine2.style.transform = "rotate(180deg)";
+        bot2.style.opacity = "1";
         currentTeam = 2;
     }
     else if (currentTeam == 2)
@@ -24,6 +27,7 @@ teamSwitch.addEventListener("click", () => {
         aLine2.style.backgroundColor = "var(--bg-color-dark)";
         aLine1.style.transform = "rotate(90deg)";
         aLine2.style.transform = "rotate(-360deg)";
+        bot2.style.opacity = "0";
         currentTeam = 1;
     }
 })
